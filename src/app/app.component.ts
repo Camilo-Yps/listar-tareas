8229490c4031c695765d6b086038a5655f052448
Camilo-Yps <camylo042012@gmail.com>
2024-12-16 13:39:56 -0500

    Funcionalidad translate agregada.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  TranslateService,
+  TranslatePipe,
+  TranslateDirective,
+  TranslateModule
+} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TranslatePipe, TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'listar-tareas';
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang('en');
+  }
+
+  switch_language(lng: string){
+    this.translate.use(lng);
+  }
 }
